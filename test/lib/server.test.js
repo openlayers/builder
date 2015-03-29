1a756f4c61f51051a1a85eba423950f9dcfd8ec0
Tim Schaub <tim.schaub@gmail.com>
2015-03-29 23:12:44 +0200

    Release fixtures for tests

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -1,14 +1,29 @@
+var path = require('path');
+
 var lab = exports.lab = require('lab').script();
 var expect = require('code').expect;
 
+var config = require('../../lib/config');
 var server = require('../../lib/server');
 
 lab.experiment('server', function() {
 
+  var releaseRoot;
+  lab.before(function(done) {
+    releaseRoot = config.releaseRoot;
+    config.releaseRoot = path.join(__dirname, '..', 'fixtures', 'releases');
+    done();
+  });
+
+  lab.after(function(done) {
+    config.releaseRoot = releaseRoot;
+    done();
+  });
+
   lab.test('GET / includes CORS headers', function(done) {
     var options = {
       method: 'GET',
-      url: '/'
+      url: server.lookup('root').path
     };
 
     server.inject(options, function(response) {
@@ -21,13 +36,31 @@ lab.experiment('server', function() {
   lab.test('GET / redirects to /releases/', function(done) {
     var options = {
       method: 'GET',
-      url: '/'
+      url: server.lookup('root').path
     };
 
     server.inject(options, function(response) {
       expect(response.statusCode).to.equal(302);
       expect(response.headers.location).to.equal(
           server.lookup('releases').path);
+      done();
+    });
+  });
+
+  lab.test('GET /releases/', function(done) {
+    var options = {
+      method: 'GET',
+      url: server.lookup('releases').path
+    };
+
+    server.inject(options, function(response) {
+      var result = response.result;
+      expect(response.statusCode).to.equal(200);
+      expect(result).to.include('releases');
+      expect(result.releases).to.be.instanceof(Array);
+      expect(result.releases).to.have.length(1);
+      expect(result.releases[0]).to.include('name');
+      expect(result.releases[0].name).to.equal('1.2.3');
       done();
     });
   });
