0eb833e8fcacb666c0b359f2cb5e6d38b5c296eb
Tim Schaub <tim.schaub@gmail.com>
2015-03-29 22:49:16 +0200

    Archive URL

--- test/lib/release.test.js
+++ test/lib/release.test.js
@@ -7,16 +7,16 @@ lab.experiment('Release', function() {
 
   lab.test('constructor', function(done) {
     var name = '3.4.5';
-    var url = 'http://example.com/release.zip';
+    var archive = 'http://example.com/release.zip';
 
     var release = new Release({
       name: name,
-      url: url
+      archive: archive
     });
 
     expect(release).to.be.an.instanceof(Release);
     expect(release.name).to.equal(name);
-    expect(release.url).to.equal(url);
+    expect(release.archive).to.equal(archive);
     expect(release.state).to.equal(Release.states.PENDING);
     expect(release.created).to.be.an.instanceof(Date);
     expect(release.updated).to.be.an.instanceof(Date);
@@ -33,29 +33,29 @@ lab.experiment('Release', function() {
 
   lab.test('constructor - no name', function(done) {
     var throws = function() {
-      return new Release({url: 'foo'});
+      return new Release({archive: 'foo'});
     };
     expect(throws).to.throw(Error, 'Missing name property');
     done();
   });
 
-  lab.test('constructor - no url', function(done) {
+  lab.test('constructor - no archive', function(done) {
     var throws = function() {
       return new Release({name: 'foo'});
     };
-    expect(throws).to.throw(Error, 'Missing url property');
+    expect(throws).to.throw(Error, 'Missing archive property');
     done();
   });
 
   lab.test('Release.fromString()', function(done) {
     var name = '1.2.3';
-    var url = 'http://example.com/release.zip';
+    var archive = 'http://example.com/release.zip';
     var state = Release.states.COMPLETE;
     var created = 10;
     var updated = 20;
     var str = JSON.stringify({
       name: name,
-      url: url,
+      archive: archive,
       state: state,
       created: created,
       updated: updated
@@ -64,7 +64,7 @@ lab.experiment('Release', function() {
     var release = Release.fromString(str);
     expect(release).to.be.an.instanceof(Release);
     expect(release.name).to.equal(name);
-    expect(release.url).to.equal(url);
+    expect(release.archive).to.equal(archive);
     expect(release.state).to.equal(state);
     expect(release.created).to.be.an.instanceof(Date);
     expect(release.created.getTime()).to.equal(created);
